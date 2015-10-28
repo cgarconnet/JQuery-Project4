322e76299e6df3bd7244cefe045caad1c3c7c00e
Chris a Aix <cgarconnet@gmail.com>
2015-10-29 00:58:44 +0100

    Now link to google maps is on

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	   success: function(response) {
 	     $.each(response.apartments, function(i, apartment) {
 	     	var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
-	       	var listing = "<a href='#' class='list-group-item "+ apartmentClass + " listings'><h4 class='list-group-item-heading'>"+ apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + " / " + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+	       	var listing = "<a href='#' id="+ apartment.id + " class='list-group-item "+ apartmentClass + " listings'><h4 class='list-group-item-heading'>"+ apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + " / " + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
 	       	$(".apartments").append(listing);
 	     });
 	    }
@@ -26,14 +26,41 @@ $(document).ready(function() {
 		// then for them we want to hide values
 			$(".listings").not("." + city).css("display", "none");
 		}
-			//we have to remove the active, as we don't which one has it, we will remove to all
-			$(".filter").removeClass("active");
+		//we have to remove the active, as we don't which one has it, we will remove to all
+		$(".filter").removeClass("active");
 
-			// then we add class active only to this
-			$(this).addClass("active");		
+		// then we add class active only to this
+		$(this).addClass("active");		
 
 	});
 
+
+	$(document).on("click", ".listings", function(){
+		// This function will allow us to call google when clicking on each listing
+		// We have to target the document and not the .listing.click .. because such 
+		// element do not exist when the document is first loaded
+
+//		console.log($(this).attr("id"));
+		var id = $(this).attr("id"); //we get the Id we just collected in the code above under listing
+
+	   $.ajax({
+		   url: "https://api.myjson.com/bins/2sadq?pretty=1",
+		   dataType: "json",
+		   error: function(error) {
+		    console.log(error); 
+		   },
+		   success: function(response) {
+		   		var selectedApartment = $.grep(response.apartments, function(apartment){
+		   			return apartment.id == id;
+		   		})
+		   		//debugger;
+		   		// before selected Apartment was an array of Object.
+		   		// so we can access the Object thru selectedApartment[0]
+		   		var address = selectedApartment[0].address;
+		   		window.open("http://map.google.com?q=" + address);
+		     }
+		});
+	});
 });
 
 
