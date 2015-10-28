8b50dcd62d7a6aabd80eef39ec96d7de06ea5868
Chris a Aix <cgarconnet@gmail.com>
2015-10-29 00:10:03 +0100

    Now we check the click on a filter class
    and we are ready to add the city class for each line of our JSon file

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ $(document).ready(function() {
 	   },
 	   success: function(response) {
 	     $.each(response.apartments, function(i, apartment) {
-	       	var listing = "<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>"+ apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + " / " + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
-	       	$('.apartments').append(listing);
+	     	var apartmentClass = apartment.city.toLowerCase();
+	       	var listing = "<a href='#' class='list-group-item listing'><h4 class='list-group-item-heading'>"+ apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + " / " + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+	       	$(".apartments").append(listing);
 	     });
 	    }
 	})
+
+// Now we check the click on a filter
+	$(".filter").click(function({
+		var city = $(this).attr("id"); // on chat we click, we get the attribute Id
+
+		//if not selected city as a class so we put "." +
+		// then for them we want to hide values
+		$(".listing").not("." + city).css("display", "none");
+
+	});
 
 });
 
